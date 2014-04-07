c96eab09b4bc27dbbc098903b753a25a2ebe8fb5
alrutten <arutten@orn.mpg.de>
2014-04-07 11:54:18 +0200

    re-initialise

---
+++ R/EVENTS.R
@@ -0,0 +1,148 @@
+
+sql <- function(con, statement) {
+  res = mysqlQuickSQL(con, statement = statement)
+  return(res)
+  
+}
+assignEvents = function(d,maxt = 2.1) {
+  #d must have datetime_, r_pk, transp, box, bout_length, LB
+  # returns d plus boutID
+  # RUN PER BOX
+  if (length(unique(d$box))>1) stop('run this per box (use lapply/ddply')
+  d = d[order(d$r_pk),]
+  keepvars = c(names(d),'boutID','filltransp')
+  d$datetime_ = as.POSIXct(d$datetime_,tz='UTC')
+  d$timePrev=c(d$datetime_[1],d$datetime_[-nrow(d)])
+  d$timenxt=c(d$datetime_[-1],d$datetime_[nrow(d)])
+  d$blPrev=c(d$bout_length[1],d$bout_length[-nrow(d)])
+  timeout    = as.POSIXct(d$timePrev,tz='UTC')<(as.POSIXct(d$datetime_,tz='UTC')-maxt)
+  
+  
+  
+  if(is.na(d$transp[1])) d$transp[1]='foo'
+  d$transp[timeout] = 'ta' #make sure transp does not spill over into next visit
+  d$filltransp=na.locf(d$transp,na.rm=FALSE)
+  d$filltransp[d$filltransp=='ta'] = NA
+  d$filltransp=na.locf(d$filltransp,na.rm=FALSE,fromLast = TRUE)
+  d$transp[d$transp%in%c('foo','ta')] = NA
+  
+  
+  d$transpPrev=c('foo',d$filltransp[-nrow(d)])
+  
+  
+  # bout start/stop criteria: different transponder, drop in boutlength, increase in boutlength, reading over maxt seconds after previous
+  
+  othertransp = d$filltransp!=d$transpPrev&(d$filltransp!='foo'&d$transpPrev!='foo')
+  bldrop     = d$bout_length<d$blPrev-20
+  blinc      = d$bout_length>d$blPrev+20
+  
+  d$boutID = 0
+  d$boutID[timeout|bldrop|blinc|othertransp] = 1
+  d$boutID[1] = 1
+  d$boutID = cumsum(d$boutID)
+  
+  # reset transp to NA when there is not actually a transponder read
+  filltransp = ddply(d,.(boutID),summarise, filltransp = unique(transp[!is.na(transp)]))
+  d$filltransp = NULL
+  d = merge(d,filltransp,all.x=TRUE)
+  
+  
+  
+  return(d[order(d$r_pk),keepvars])
+}
+
+
+extractEvents = function(d, min_t = 2.1) {
+  
+  newLB = c('10','12','02')
+  d$LB = match(d$LB,newLB)
+  d$LB = d$LB-2
+  #d$LB[is.na(d$LB)] = 0
+  d=d[order(d$r_pk),]
+  events=ddply(d[d$boutID>0,],.(box,id,boutID),summarise, transp=filltransp[1],
+               startt=min(datetime_),
+               endt=max(datetime_),
+               duration = as.numeric(difftime(max(datetime_),min(datetime_), tz='UTC', units = 'secs')),
+               nline_beforetransp =which(!is.na(transp))[1],
+               first_LB=LB[which(!is.na(LB))][1],
+               last_LB = tail(c(NA,LB[which(!is.na(LB))]),1),
+               anyLB12 = any(LB==0,na.rm=TRUE),
+               n_lines=length(LB),
+               mean_bl = mean(bout_length),
+               sum_bl=as.numeric(sum(bout_length)),
+               ratio_l_bl = n_lines/sum_bl)
+  # discard events where there's no transit and no transponder
+  events=events[!(!events$anyLB12&is.na(events$transp)),] 
+  
+  events$LBdir = events$last_LB - events$first_LB
+  events$LBdir[is.na(events$LBdir)] = 0
+  #do previouses per transponder
+  events = events[order(events$transp,events$startt),]
+  prevevents = ddply(events,.(transp),  function(x) rbind(x[1,], x[-nrow(x),]))		
+  names(prevevents) = paste0('prev_',names(events)) 
+  events = cbind(events,prevevents[,c('prev_n_lines','prev_sum_bl','prev_startt','prev_transp','prev_LBdir')])
+  events$ratio_l_prevl = events$n_lines/events$prev_n_lines
+  return(events[order(events$startt),])
+}  
+
+birdIDs = function() {
+  con      = dbConnect(dbDriver("MySQL"), user = "snb", password = "cs", host = "scidb.orn.mpg.de", dbname = 'SNBatWESTERHOLZ2')
+  on.exit(dbDisconnect(con))
+  
+  d = sql(con,"SELECT x.*, s.sex from 
+          (SELECT distinct ID birdID, transponder, FUNCTIONS.combo(UL,LL,UR,LR) cb from BTatWESTERHOLZ.ADULTS where transponder IS NOT NULL
+          UNION SELECT distinct c.ID birdID, c.transponder, FUNCTIONS.combo(a.UL,a.LL,a.UR,a.LR) cb from BTatWESTERHOLZ.CHICKS c 
+          LEFT JOIN BTatWESTERHOLZ.ADULTS a on c.ID = a.ID where c.transponder IS NOT NULL
+          UNION SELECT distinct ID birdID, transponder, FUNCTIONS.combo(UL,LL,UR,LR) cb from FIELD_BTatWESTERHOLZ.ADULTS where transponder IS NOT NULL
+          UNION SELECT 'fieldteam' birdID, transponder, 'fieldteam' cb from SNBatWESTERHOLZ2.test_transponders 
+          )x
+          left join BTatWESTERHOLZ.SEX s on x.birdID = s.ID
+          
+          order by transponder
+          "
+  )
+  
+  return(d)
+}
+loadEvents = function(year_=substring(Sys.Date(),1,4)) {
+
+  con      = dbConnect(dbDriver("MySQL"), user = "snb", password = "cs", host = "scidb.orn.mpg.de", dbname = 'SNBatWESTERHOLZ2')
+  on.exit(dbDisconnect(con))
+  IDs = birdIDs()
+  load("/home/snb/snb_io/data/ct.Rdata")
+  vars = paste0("c('",paste(strsplit(gsub(' ','',paste(ct@data@formula$input)),'\\+')[[2]],collapse='\',\''),"')")
+  keepvars = names(sql(con,'select * from BETA_Events2014 limit 1'))
+  #fids = sql(con,'select id from file_status where year_ = 2013 and box between 100 and 201 order by id desc')$id
+  foo = try({fids = sql(con, paste0("select f.id from file_status f 
+                                    LEFT JOIN (select distinct id from BETA_Events",year_,") b on f.id = b.id 
+                                    where b.id IS NULL and year_ = ",year_," order by id desc"))$id},silent=TRUE)
+  if ((!class(foo)=='try-error')&length(fids)>0) {
+    for (i in 1:length(fids)) {
+      d = sql(con, paste0("select * from RAW_",year_," where id = ",fids[i]))
+      if (nrow(d)>0) {
+        nulls = d[is.na(d$datetime_),]
+        if (nrow(nulls) > 0) {
+          nulls = ddply(nulls,.(box,datetime_,transp,id),summarise, count = -1)
+          nulls = merge(nulls,IDs, by.x='transp',by.y='transponder',all.x=TRUE,incomparables = NA) 
+          nulls[,setdiff(keepvars, names(nulls))] = NA
+          dbWriteTable(con,paste0('BETA_Events',year_),nulls[,keepvars],row.names=FALSE,append=TRUE)
+        }
+        d = d[!is.na(d$datetime_),]
+        if (nrow(d)>0) {
+          dd = assignEvents(d)
+          dd = extractEvents(dd)
+        if (nrow(dd)>0) {
+          dd$direction_raw = round(predict(ct,newdata = dd[,eval(parse(text=vars))]),2)
+          dd$direction = round(dd$direction_raw,0)
+          dd$direction[abs(dd$direction_raw-dd$direction)>0.1] = 0
+                 
+          dd = merge(dd,IDs, by.x='transp',by.y='transponder',all.x=TRUE,incomparables = NA) 
+        
+          dbWriteTable(con,paste0('BETA_Events',year_),dd[,keepvars],row.names=FALSE,append=TRUE)
+        }
+      }
+    }
+  }
+  }
+}
+
---
+++ R/datacopyGUI.R
@@ -0,0 +1,179 @@
+
+  
+  checkSD = function(con, SDsn) {
+    dc = FALSE
+    if (missing(con)) {
+      con = dbcon(database = 'SNBatWESTERHOLZ2',username='snb',password = 'cs')
+      on.exit(mysqlCloseConnection(con))
+    }
+    check=sql(con,paste("SELECT sum(SD_K_status > 0) n_bad, 
+                        sum(SD_K_status>0)/count(SD_K_status) ratio, count(SD_K_Status) total 
+                        from TECHatWESTERHOLZ.technical_details
+                        where SD_K_Status IS NOT NULL
+                        and SD_ID=",shQuote(SDsn),sep="") 
+    )
+    msg = paste("Card ID ",SDsn)
+    return(ifelse(check$ratio>0.27 & check$total>4,paste(msg,"\nDESTROY THIS CARD"),paste(msg,"\ncard used",check$total,"times previously")))	
+    
+  }  
+  
+  getSD = function(snbDir = '//ds/raw_data_kemp/FIELD/Westerholz/SNB/RAWDATA',dtime,bx,filename="BOX001.TXT" ) {
+    
+    con = dbcon(database = 'SNBatWESTERHOLZ2',username='snb',password = 'cs')
+    
+    outname = paste(substr(dtime,1,4),substr(dtime,1,10),bx,"box001.txt",sep="/")
+    outfile = paste(snbDir,outname,sep='/')
+    dtime=gsub("\\.","-",dtime)
+    drv=findRemovable()
+    prev = sql(con,paste("SELECT max(date_time_field) md from TECHatWESTERHOLZ.technical_details where box = ",bx," and SD_K_Status IS NOT NULL and date_time_field<",shQuote(dtime)))$md
+    
+    
+    if (!(file.exists(outfile))) { 
+      dir.create(dirname(outfile),showWarnings=FALSE,recursive=TRUE)
+      
+      
+      
+      if (length(drv)>0)  {   # SD card gets detected
+        filename=paste(drv[1],"\\",filename,sep="")
+        Sdsn=getSDserial(drv)
+        out=checkSD(con=con,SDsn=Sdsn)
+        if (file.exists(filename)){        # file present
+          insize=file.info(filename)$size   # this information is no longer accessible after trying file.copy because of i/o errors    
+          file.copy(filename,outfile,overwrite=FALSE) #sadly, file.copy() returns TRUE for partial copies
+          
+          sql(con,paste("INSERT INTO SNBatWESTERHOLZ2.file_status (path,year_,date_,date_prev,filesize,box,upload_status,boxversion) VALUES('",outname,"',",substr(dtime,1,4),
+                        ",'",dtime,"',",shQuote(prev),",",file.info(outfile)$size/1000,",",bx,",0,0)",sep=""))
+          
+          if (insize == file.info(outfile)$size) {
+            sql(con,paste("INSERT INTO TECHatWESTERHOLZ.technical_details (date_time_field,box,SD_K_Status,SD_K_Aktion,SD_ID) VALUES('",dtime,"','",bx,"','0','0','",Sdsn,"')",sep=""))
+            out=paste("file copied without problems\n",out,'\n')
+          } else{	
+            sql(con,paste("INSERT INTO TECHatWESTERHOLZ.technical_details (date_time_field,box,SD_K_Status,SD_K_Aktion,SD_ID) VALUES('",dtime,"','",bx,"','5','3','",Sdsn,"')",sep=""))
+            out=paste("card damaged, file partly recovered\n",out,"\n")
+          } } else{
+            sql(con,paste("INSERT INTO TECHatWESTERHOLZ.technical_details (date_time_field,box,SD_K_Status,SD_ID) VALUES('",dtime,"','",bx,"','3','",Sdsn,"')",sep=""))
+            out=paste("No file on card\n",out,"\n")
+          } } else {	sql(con,paste("INSERT INTO TECHatWESTERHOLZ.technical_details (date_time_field,box,SD_K_Status,SD_ID) VALUES('",dtime,"','",bx,"','1','NULL')",sep=""))
+                     out=paste("CAUTION: card not detected")
+          }	
+    } else out=paste('ERROR: destination file ',outfile,' already exists. File not copied!',sep="")
+    on.exit(mysqlCloseConnection(con))
+    cat(out)
+    return(out)
+  }
+  
+  
+  
+  getHandheld = function(snbDir = '//ds/raw_data_kemp/FIELD/Westerholz/SNB/RAWDATA',dtime, filename="box001.txt") {
+    
+    con = dbcon(database = 'SNBatWESTERHOLZ2',username='snb',password = 'cs')
+    
+    drv=findRemovable()
+    path=paste(drv[1],"\\",dtime,sep="")
+    
+    flist=list.files(path, recursive = TRUE, full.names = TRUE) 
+    boxlist=basename(dirname(flist))
+    errlist=''
+    if (length(flist)==0) paste("nothing to upload") else
+    {
+      for (i in (1:length(flist))) {
+        outdir=paste(snbDir,substr(dtime,1,4),paste(substr(dtime,1,10),"CF",sep=""),tolower(boxlist[i]),sep="/")
+        mtime=file.info(flist[i])$mtime
+        prev = sql(con,paste("SELECT max(date_time_field) md from TECHatWESTERHOLZ.technical_details where box = ",tolower(boxlist[i])," and SD_K_Status IS NOT NULL and date_time_field<",shQuote(mtime)))$md
+        dir.create(outdir,recursive=TRUE)
+        if (!file.exists(paste(outdir,filename,sep="/"))) {
+          file.copy(flist[i],outdir,overwrite=FALSE)
+          sql(con,paste("INSERT INTO TECHatWESTERHOLZ.technical_details (date_time_field,box,SD_K_Status, SD_K_Aktion) VALUES('",mtime,"','",boxlist[i],"','0','0')",sep=""))
+          sql(con,paste0("INSERT INTO SNBatWESTERHOLZ2.file_status (path,year_,date_,date_prev,filesize,box,upload_status,boxversion) VALUES('",paste(gsub(snbDir,'',outdir),filename,sep='/'),"',",substr(dtime,1,4),
+                         ",'",mtime,"',",shQuote(prev),",",round(file.info(flist[i])$size/1000),",",boxlist[i],",0,0)"))
+          
+        } else errlist=paste(errlist,boxlist[i],sep=',')
+      }
+      paste(i,' files processed',ifelse(errlist!='',paste('; NB: destination files for box(es) ',errlist,' already existed. THOSE FILES NOT COPIED',sep=""),''),sep="")
+    }
+    on.exit(mysqlCloseConnection(con))
+  }	
+  
+  
+  
+  
+  getSDGui=function(dte=format(Sys.time()-86400,"%Y.%m.%d %H:%M:%S")) {
+    
+    sdw=tktoplevel()
+    
+    boxsel=tclVar("666")
+    datesel=tclVar(dte)
+    
+    
+    
+    
+    boxentry  = tkentry(sdw, textvariable=boxsel)
+    boxlabel  = tklabel(sdw,text="box (all 3 digits):")
+    dateentry = tkentry(sdw,textvariable=datesel)
+    datelabel = tklabel(sdw,text="datetime ")
+    
+    OnOk = function(bx=tclvalue(boxsel),dt_=tclvalue(datesel)){
+      # if (missing(con)) con=snbcon()
+      cardThere = findRemovable()
+      if (length(cardThere)>1) tkmessageBox(message = "remove all USB storage devices that are not the SD-card you want to copy. Now.")
+      if(length(cardThere)==0) foo = tkmessageBox(message="no card detected. Proceed?\n (yes= after inserting card (or if it's already there)\n no= cancel upload",type='yesno') else 
+        foo = tclVar('yes')
+      if (tclvalue(foo)=='yes'){
+        txt = getSD(dtime=dt_,bx=bx)
+        tkmessageBox(message=paste(txt))
+      } else tkmessageBox(message="upload canceled")
+    }
+    
+    OK.but <-tkbutton(sdw,text="START UPLOAD",command=function() OnOk())
+    Cancel.But =tkbutton(sdw,text="CANCEL/DONE",command=function() tkdestroy(sdw))
+    tkgrid(boxlabel,boxentry,datelabel,dateentry)
+    tkgrid(tklabel(sdw,text="    "))
+    tkgrid(OK.but,Cancel.But) 
+    tkgrid.configure(OK.but,column=1)
+    tkgrid.configure(Cancel.But,column=2)
+  }
+  
+  
+  getHandheldGui=function(dte=format(Sys.Date()-1,"%Y.%m.%d")) {
+    hhw=tktoplevel()
+    
+    datesel=tclVar(dte)
+    dateentry = tkentry(hhw,textvariable=datesel)
+    datelabel=tklabel(hhw,text="date ")
+    
+    OnOk=function(){
+      cardThere=findRemovable()
+      if(length(cardThere)==0) foo=tkmessageBox(message="no card detected. Proceed?\n (no= cancel upload)",type='yesno') else foo=tclVar('yes')
+      if (tclvalue(foo)=='yes'){
+        txt=getHandheld(dtime=tclvalue(datesel))
+        tkmessageBox(message=paste(txt))
+      } else tkmessageBox(message="upload canceled")
+    }
+    OK.but <-tkbutton(hhw,text="START UPLOAD",command=function() OnOk())
+    Cancel.But =tkbutton(hhw,text="CANCEL/DONE",command=function() tkdestroy(hhw))
+    tkgrid(datelabel,dateentry)
+    tkgrid(tklabel(hhw,text="    "))
+    tkgrid(OK.but,Cancel.But) 
+    tkgrid.configure(OK.but,column=1)
+    tkgrid.configure(Cancel.But,column=2)
+  }
+  
+  findRemovable = function() { #uses wmic to list all removable drives (fsutil needs admin power)
+    
+    drvLetter =paste(unique(gsub("[^A-Z]","",(system('wmic logicaldisk get caption',intern=TRUE)))),":",sep="")
+    drvUsed=as.numeric(gsub("\\D","",system('wmic logicaldisk get freespace',intern=TRUE))[-1])
+    drvType=as.numeric(gsub("\\D","",system('wmic logicaldisk get drivetype',intern=TRUE))[-1])
+    
+    drv=as.data.frame(cbind(drvLetter,drvUsed,drvType),stringsAsFactors=FALSE)
+    
+    return(drv$drvLetter[which(!is.na(drv$drvUsed)&drv$drvType==2)])
+    
+    
+  }
+  
+  getSDserial = function(SDdir) { 
+    foo=shell(paste("dir ",SDdir,sep=""),intern=TRUE)
+    foo=gsub(".*is\ ","",foo[2])
+    return(foo)
+  }
+  
